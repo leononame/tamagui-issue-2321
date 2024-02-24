d001153f4efb8b06c3b31dd097935ca5f11a44d5
Leo <leononame@lho.io>
2024-02-24 21:22:02 +0100

    compare with rnw textinput

--- packages/app/features/user/detail-screen.tsx
+++ packages/app/features/user/detail-screen.tsx
@@ -3,6 +3,8 @@ import { ChevronLeft } from '@tamagui/lucide-icons'
 import React from 'react'
 import { createParam } from 'solito'
 import { useLink } from 'solito/link'
+import { Input } from '@my/ui'
+import { TextInput } from 'react-native'
 
 const { useParam } = createParam<{ id: string }>()
 
@@ -15,6 +17,15 @@ export function UserDetailScreen() {
   return (
     <YStack f={1} jc="center" ai="center" space>
       <Paragraph ta="center" fow="700">{`User ID: ${id}`}</Paragraph>
+      <Input
+        onBlur={event => console.log('tamagui onBlur', event.nativeEvent.text)}
+        onEndEditing={event => console.log('tamagui onEndEditing', event.nativeEvent.text)}
+      />
+      <TextInput
+        style={{borderColor: "red", borderWidth: 1}}
+        onBlur={event => console.log('rnw onBlur', event.nativeEvent.text)}
+        onEndEditing={event => console.log('rnw onEndEditing', event.nativeEvent.text)}
+      />
       <Button {...link} icon={ChevronLeft}>
         Go Home
       </Button>
